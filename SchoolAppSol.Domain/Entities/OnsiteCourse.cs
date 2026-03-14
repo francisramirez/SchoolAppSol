140e5b8aa971d802d0587850038d94f6f6f68988
Francis Ramirez <Francis Ramirez@LAPTOP-DF01N7UN>
2026-03-13 23:38:00 -0400

    ok.

--- SchoolAppSol.Domain/Entities/OnsiteCourse.cs
+++ SchoolAppSol.Domain/Entities/OnsiteCourse.cs
@@ -1,9 +1,14 @@
 ﻿#nullable disable
 
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace SchoolAppSol.Domain.Entities;
 
+[Table("OnsiteCourse")]
 public partial class OnsiteCourse : Base.Entity<int>
 {
+    [Key]
     public int CourseId { get; set; }
 
     public string Location { get; set; }
