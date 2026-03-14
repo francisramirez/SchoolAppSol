24283986c34ceb11844361afac630778262ee96a
Francis Ramirez <Francis Ramirez@LAPTOP-DF01N7UN>
2026-03-13 23:15:57 -0400

    refactor: remove `chat-input-refactor` feature flag and associated conditional rendering logic.

--- SchoolAppSol.Api/Program.cs
+++ SchoolAppSol.Api/Program.cs
@@ -28,6 +28,8 @@ namespace SchoolAppSol.Api
             builder.Services.AddScoped<ICourseDomainRepository, CourseRepository>();
             builder.Services.AddScoped<IDepartmentDomainRepository, DepartmentRepository>();
             builder.Services.AddScoped<IDepartmentRepository, DepartmentRepository>();
+            builder.Services.AddScoped<IOnsiteCourseRepository, OnsiteCourseRepository>();
+            builder.Services.AddScoped<IOnsiteCourseDomainRepository, OnsiteCourseRepository>();
             builder.Services.AddScoped<ICourseValidator, CourseValidator>();
             builder.Services.AddScoped<IDepartmentValidator, DepartmentValidator>();
             builder.Services.AddScoped<ICourseService, CourseService>();
