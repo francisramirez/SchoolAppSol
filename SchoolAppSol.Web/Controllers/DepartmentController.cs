19c653ab6bef654577c7c1b614fd4dabcd476841
Francis Ramirez <Francis Ramirez@LAPTOP-DF01N7UN>
2026-03-24 18:53:03 -0400

    feat: Implement initial application structure for student, course enrollment, and user management.

---
+++ SchoolAppSol.Web/Controllers/DepartmentController.cs
@@ -0,0 +1,152 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SchoolAppSol.Application.Base;
+using SchoolAppSol.Application.Interfaces.Department;
+using SchoolAppSol.Domain.Models;
+using SchoolAppSol.Application.Dtos.Department;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SchoolAppSol.Web.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        private readonly IDepartmentService _departmentService;
+
+        public DepartmentController(IDepartmentService departmentService)
+        {
+            _departmentService = departmentService;
+        }
+
+        // GET: DepartmentController
+        public async Task<ActionResult> Index()
+        {
+            ServiceResult<List<DepartmentModel>> result = new ServiceResult<List<DepartmentModel>>();
+
+            result = await _departmentService.GetAllDepartmentsAsync();
+
+            if (!result.Success)
+            {
+                ViewBag.Message = result.Message;
+                return View();
+            }
+
+            return View(result.Data);
+        }
+
+        // GET: DepartmentController/Details/5
+        public async Task<ActionResult> Details(int id)
+        {
+            ServiceResult<DepartmentModel> result = new ServiceResult<DepartmentModel>();
+
+            result = await _departmentService.GetDepartmentByIdAsync(id);
+
+            if (!result.Success)
+            {
+                ViewBag.Message = result.Message;
+                return View();
+            }
+
+            return View(result.Data);
+        }
+
+        // GET: DepartmentController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: DepartmentController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create(DepartmentAddDto collection)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return View(collection);
+                }
+
+                var result = await _departmentService.CreateDepartmentAsync(collection);
+
+                if (!result.Success)
+                {
+                    ViewBag.Message = result.Message;
+                    return View(collection);
+                }
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: DepartmentController/Edit/5
+        public async Task<ActionResult> Edit(int id)
+        {
+            ServiceResult<DepartmentModel> result = new ServiceResult<DepartmentModel>();
+
+            result = await _departmentService.GetDepartmentByIdAsync(id);
+
+            if (!result.Success)
+            {
+                ViewBag.Message = result.Message;
+                return View();
+            }
+
+            return View(result.Data);
+        }
+
+        // POST: DepartmentController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit(int id, UpdateDepartmentDto collection)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return View(collection);
+                }
+
+                var result = await _departmentService.UpdateDepartmentAsync(id, collection);
+
+                if (!result.Success)
+                {
+                    ViewBag.Message = result.Message;
+                    return View(collection);
+                }
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: DepartmentController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: DepartmentController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
