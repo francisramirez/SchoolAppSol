f9ad12d305b58ba3dfa833c4c02b9e686c4e1044
Francis Ramirez <Francis Ramirez@LAPTOP-DF01N7UN>
2026-03-24 18:10:32 -0400

    asignando el pk a la propiedad EnrollmentID

--- SchoolAppSol.Domain/Entities/CourseEnrollment.cs
+++ SchoolAppSol.Domain/Entities/CourseEnrollment.cs
@@ -1,10 +1,12 @@
 ﻿
 using SchoolAppSol.Domain.Base;
+using System.ComponentModel.DataAnnotations;
 
 namespace SchoolAppSol.Domain.Entities;
 
 public partial class CourseEnrollment : AuditableEntity<int>
 {
+    [Key]
     public int EnrollmentId { get; set; }
 
     public int CourseId { get; set; }
